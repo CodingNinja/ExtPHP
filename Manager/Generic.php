bb46d1fd26c4e892285ca4e8b618440a765b5eb7
CodingNinja <ninja@codingninja.com.au>
2011-03-24 21:54:13 +1100

    [FEATURES/DEPRECATION/CLEANING] - Updates to the generic component manager
    This commt contains quite a bit of "code cleanup" to remove some
    deprecated methods and also formatted the code.
    New features include the ability to "postRender" a component
    and a example of this has also been added (Render a minified version).
    
    Also contains methods which have been removed from PHPJs::Manager as they
    are ExtPHP specific methods.

--- Manager/Generic.php
+++ Manager/Generic.php
@@ -12,9 +12,9 @@
 
 namespace ExtPHP\Manager;
 
-use \ExtPHP\Component;
-use \PHPJs\Manager;
-use \PHPJs\Converter\MethodCall;
+use ExtPHP\Component;
+use PHPJs\Manager;
+use PHPJs\Converter\MethodCall;
 
 /**
  * Generic Output Manager
@@ -24,9 +24,9 @@ use \PHPJs\Converter\MethodCall;
  *
  * Example usage:
  * <code>
- *  $component = \PHPJs\Manager\Generic::componentFromXtype('panel', $config);
- *  $manager = \PHPJs\Manager\Generic::dashboard()->register($component);
- *  $manaer->getOutput();
+ * $component = \PHPJs\Manager\Generic::componentFromXtype('panel', $config);
+ * $manager = \PHPJs\Manager\Generic::dashboard()->register($component);
+ * $manaer->getOutput();
  * </code>
  *
  * @package     PHPJs
@@ -36,93 +36,104 @@ use \PHPJs\Converter\MethodCall;
  */
 class Generic extends Manager
 {
-
-  protected $outputWrapper = "%s\n\nExt.onReady(function() {\n%s%s%s\n});";
-
-  /**
-   * Render a manager
-   *
-   * Render the manager instance
-   *
-   * @param string $prefix Code to place before the rendered javascript
-   * @param string $suffix Code to place after the rendered javascript
-   * @return string The rendered manager instance
-   */
-  public function render($prefix = '', $suffix = '')
-  {
-    $output = '';
-
-    foreach ($this->components as $component)
-    {
-      $output .= $this->renderItem($component);
+    public static $obfuscate = false;
+    
+    protected $outputWrapper = "%s\n\nExt.onReady(function() {\n%s%s%s\n});";
+    
+    /**
+     * Render a manager
+     *
+     * Render the manager instance
+     *
+     * @param string $prefix Code to place before the rendered javascript
+     * @param string $suffix Code to place after the rendered javascript
+     * @return string The rendered manager instance
+     */
+    public function render($prefix = '', $suffix = '') {
+        $output = '';
+        
+        foreach ( $this->components as $component ) {
+            $output .= $this->renderItem ( $component );
+        }
+        
+        return $this->postRender ( sprintf ( $this->getOutputWrapper (), $this->getAllVarNames (), ($prefix ? $prefix . "\n" : ''), $output, ($suffix ? $suffix . "\n" : '') ) );
     }
-
-    return sprintf($this->getOutputWrapper(), $this->getAllVarNames(), ($prefix ? $prefix .
-      "\n" : ''), $output, ($suffix ? $suffix . "\n" : ''));
-  }
-
-  public function renderItem($component)
-  {
-    $output = '';
-    if($component instanceof Component) {
-      $output .= $this->renderDependencies($component);
-      $output .= $this->renderComponent($component);
-    }elseif($component instanceof MethodCall) {
-      $output .= $this->renderDependencies($component->getObject());
+    
+    public function postRender($contents) {
+        if (! self::$obfuscate) {
+            return $contents;
+        }
+        return preg_replace ( "/([ ]{2,}|[\t]|[\r]|[\n])/", '', $contents );
     }
-
-    return $output;
-  }
-
-  /**
-   * Get all var names
-   *
-   * Get's all the variable names that are contained within this manager
-   *
-   * @throws InvalidMethodCallException Thrown when the current manager has not been rendered
-   * @return string Variable containing all the variable names
-   */
-  public function getAllVarNames()
-  {
-    if (!sizeof($this->rendered))
-    {
-      throw new \BadMethodCallException('Manager must be rendered to retrieve variable names');
+    
+    public function renderItem($component) {
+        $output = '';
+        if ($component instanceof Component) {
+            $output .= $this->renderDependencies ( $component );
+            $output .= $this->renderComponent ( $component );
+        } elseif ($component instanceof MethodCall) {
+            $output .= $this->renderDependencies ( $component->getObject () );
+        }
+        
+        return $output;
     }
-
-    return 'var ' . implode(",\n", array_keys($this->rendered)) . ';';
-  }
-
-  /**
-   * Render the dependencies
-   *
-   * Simple dependency loader which get's the dependencies of a component and
-   * render's them (if they are not already rendered)
-   *
-   * @param Component $component The component to use
-   * @return string The rendered dependencies
-   */
-  public function renderDependencies(Component $component)
-  {
-    $dependencies = '';
-    foreach ($component->getDependencies() as $dependency)
-    {
-      if ($dependency->hasDependencies())
-      {
-        $dependencies .= $this->loadDependencies($dependency);
-      }
-
-      $dependencies .= $this->renderComponent($dependency);
+    
+    /**
+     * Get all var names
+     *
+     * Get's all the variable names that are contained within this manager
+     *
+     * @throws InvalidMethodCallException Thrown when the current manager has not been rendered
+     * @return string Variable containing all the variable names
+     */
+    public function getAllVarNames() {
+        if (! sizeof ( $this->rendered )) {
+            throw new \BadMethodCallException ( 'Manager must be rendered to retrieve variable names' );
+        }
+        
+        return 'var ' . implode ( ",\n", array_keys ( $this->rendered ) ) . ';';
     }
-
-    return $dependencies;
-  }
-
-  public function getComponentClassFor($class) {
-    return '\\ExtPHP\\Component';
-  }
-
-  public function methodCall()
-  {
-
-  }
+    
+    /**
+     * Render the dependencies
+     *
+     * Simple dependency loader which get's the dependencies of a component and
+     * render's them (if they are not already rendered)
+     *
+     * @param Component $component The component to use
+     * @return string The rendered dependencies
+     */
+    public function renderDependencies(Component $component) {
+        $dependencies = '';
+        foreach ( $component->getDependencies () as $dependency ) {
+            if ($dependency->hasDependencies ()) {
+                $dependencies .= $this->renderDependencies ( $dependency );
+            }
+            
+            $dependencies .= $this->renderComponent ( $dependency );
+        }
+        
+        return $dependencies;
+    }
+    
+    public function getComponentClassFor($class) {
+        return '\\ExtPHP\\Component';
+    }
+    
+    public function methodCall() {
+    
+    }
+    
+    /**
+     * Create an XType component
+     *
+     * Proxy method to the {@link Manager::ComponentFactory()} method to include the "{@link Component::JSON}" switch
+     *
+     * @param string $xtype The XType to use when rendering the component
+     * @param array $config The configuration for the component
+     * @return Component The component that was created
+     */
+    public function componentFromXType($xtype, $config) {
+        return $this->componentFactory ( $xtype, $config, Component::JSON )->setOption('xtype', $xtype);
+    }
 }
